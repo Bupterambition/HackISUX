f454de44c27c3d2687329046f8684f05833e564d
Ben Konz <bkonz@unomaha.edu>
2018-10-20 00:47:48 -0500

    renamed some files

---
+++ farm_market/src/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+	render() {
+		return (
+			<div className="welcome">
+				<h1>Welcome to the online Farmer's Market!</h1>
+				<p>Want to buy produce cheaply and easily? Well you came to the right place.</p>
+			</div>
+		);
+	}
+}
+
+export default Welcome;
--- farm_market/src/index.js
+++ farm_market/src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-class Welcome extends React.Component {
-	render() {
-		return (
-			<div className="welcome">
-				<h1>Welcome to the online Farmer's Market!</h1>
-				<p>Want to buy produce cheaply and easily? Well you came to the right place.</p>
-			</div>
-		);
-	}
-}
 
 ReactDOM.render(
-	<Welcome />,
+	<App />,
 	document.getElementById('root')
 );
 
