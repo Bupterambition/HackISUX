f1424ecc0b5813d73e105684b5bb3ed0e8b43edb
pgayee694 <pgayee694@gmail.com>
2018-10-20 01:13:56 -0500

    Flask endpoint for specific inventory item

--- farmland/app/api.py
+++ farmland/app/api.py
@@ -8,7 +8,13 @@ def index():
     return 'Test landing page'
 
 @app.route('/farmers/<location>')
-def profile(location):
+def get_farmers_by_loc(location):
     client = MongoClient()
     db = client.database
     return dumps(db.farmers.find({"location": location}))
+
+@app.route('/inventory/<item>')
+def get_food(item):
+    client = MongoClient()
+    db = client.database
+    return dumps(db.inventory.find({"name": item}))
