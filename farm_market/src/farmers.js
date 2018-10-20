03dd8bb20e273855adb6c72ac32ecc8b1f859725
Ben Konz <bkonz@unomaha.edu>
2018-10-20 14:40:44 -0500

    removed welcome from farmers

--- farm_market/src/farmers.js
+++ farm_market/src/farmers.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Welcome from './Welcome';
 
 class Farmers extends Component {
   render() {
@@ -12,7 +11,6 @@ class Farmers extends Component {
     return (
        <div>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"></link>
-        <Welcome />
         <h3 style={h3Styles}>View Farmer&#39;s Market growers here by viewing farmer&#39;s profiles</h3>
       </div>
     );
