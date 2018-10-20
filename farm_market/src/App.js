8ca775b3b1e7b18e87a72374b3b3bfe259acc97d
Ben Konz <bkonz@unomaha.edu>
2018-10-20 01:40:59 -0500

    updated some code it sucks

--- farm_market/src/App.js
+++ farm_market/src/App.js
@@ -3,8 +3,16 @@ import Welcome from './Welcome';
 
 class App extends Component {
   render() {
+
+    const styles = {
+			"backgroundColor": "#4e7a4a"
+		};
+
     return (
-      <Welcome />
+      <div style={styles}>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"></link>
+        <Welcome />
+      </div>
     );
   }
 }
--- farm_market/src/Welcome.js
+++ farm_market/src/Welcome.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import NavHeader from './NavHeader';
 
 class Welcome extends React.Component {
 	render() {
+
+		const styles = {
+			"textAlign": "center",
+			"backgroundColor": "#ffffff"
+		};
+
 		return (
-			<div className="welcome">
-				<h1>Welcome to the online Farmer's Market!</h1>
+            <div>
+                <NavHeader />
+                <div style={styles}>
+				    <h1>Welcome to the online Farmer's Market!</h1>
 				<p>Want to buy produce cheaply and easily? Well you came to the right place.</p>
-			</div>
+			    </div>
+            </div>
 		);
 	}
 }
