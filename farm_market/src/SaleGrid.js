d2e250fc6dafbe2e76b056da2c5aeb97890f87b7
Ben Konz <bkonz@unomaha.edu>
2018-10-20 16:46:33 -0500

    added example in SaleGrid

--- farm_market/src/SaleGrid.js
+++ farm_market/src/SaleGrid.js
@@ -4,15 +4,24 @@ import SaleItem from "./SaleItem";
 
 class SaleGrid extends React.Component {
 
+    getSales() {
+        fetch("/farmers/ames").then(results => {
+            return results.json();
+        }).then(data => {
+            console.log(data);
+        });
+    }
+
     render() {
+        const styles = {
+            "backgroundColor" : "#d1d1d1",
+        }
 
-      const styles = {
-          "backgroundColor" : "#d1d1d1",
-      }
+        const panelStyles = {
+            "marginTop" : "3%"
+        }
 
-      const panelStyles = {
-        "marginTop" : "3%"
-  }
+        this.getSales();
 
         return (
             <Panel style={panelStyles}>
