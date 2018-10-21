915365bb21843bb0c37b59c9d90e7ecdffacab89
pgayee694 <pgayee694@gmail.com>
2018-10-20 21:49:52 -0500

    Broken modal on SaleItem componenet to open AddToCart stuff

--- farm_market/src/SaleItem.js
+++ farm_market/src/SaleItem.js
@@ -1,7 +1,34 @@
 import React from "react";
 import { Thumbnail, Button } from "react-bootstrap/lib";
+import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+import addToCart from './AddToCart'
 
 class SaleItem extends React.Component {
+
+    constructor() {
+        super();
+    
+        this.state = {
+          modalIsOpen: false
+        };
+    
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+      }
+
+    openModal() {
+        this.setState({modalIsOpen: true})
+    }
+
+    afterOpenModal() {
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
 
     render() {
         const buttonStyles = {
@@ -21,8 +48,13 @@ class SaleItem extends React.Component {
             <Thumbnail style={imageStyles} src="https://img1.ak.crunchyroll.com/i/spire1/91ac5d44a439598ca8f5880c0d1dd5a51471101892_large.jpg" alt="242x200">
                 <h3 style={textStyles}>{this.props.saleName}</h3>
                 <p style={buttonStyles}>
-                    <Button bsStyle="success">Buy</Button>
+                <Button bsStyle="success" onClick={this.openModal}>Add to Cart</Button>
                 </p>
+                <Modal isOpen={this.state.modalIsOpen} onAfterOpen={this.afterOpenModal} onRequestClose={this.closeModal} contentLabel="Add to Cart">
+                    {/* <h2 ref={subtitle => this.subtitle = subtitle}>Add to Cart</h2>
+                        <addtoCart/> */}
+                    <p>quack</p>
+                </Modal>
             </Thumbnail>
         );
     }
